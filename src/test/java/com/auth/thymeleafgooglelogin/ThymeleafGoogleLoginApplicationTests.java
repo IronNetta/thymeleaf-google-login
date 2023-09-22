5fdda3679d58808b458fe033e168a5c8a502820d
Sebastien De Laet <delaet.sebastien93@gmail.com>
2023-09-22 11:16:58 +0200

    Initial commit

---
+++ src/test/java/com/auth/thymeleafgooglelogin/ThymeleafGoogleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.thymeleafgooglelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafGoogleLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
